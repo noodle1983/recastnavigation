f58deaafeec143283b4e5f21d99a46056cac6814
LuDong <noodle1983@126.com>
2021-05-01 22:12:38 +0800

    fix installing path

--- NavimeshExporter/build_mac.sh
+++ NavimeshExporter/build_mac.sh
@@ -3,5 +3,5 @@
 cd proj.mac && \
 xcodebuild clean && \
 xcodebuild -parallelizeTargets -jobs 4 && \
-cp -r build/Release/NavimeshExporter.bundle ../ && \
+cp -r build/Release/NavimeshExporter.bundle ../../../Assets/NavimeshMarker/Script/Plugins/ && \
 cd -
